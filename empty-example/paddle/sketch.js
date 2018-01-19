af66ecce2eb01ee591c9a06869efa31cd11685f5
ShubhamK <shubhamkhaparey47@gmail.com>
2018-01-20 01:48:01 +0530

    ^ ^

---
+++ empty-example/paddle/sketch.js
@@ -0,0 +1,130 @@
+var p
+var b
+function setup(){
+        x=0;
+        createCanvas(window.innerWidth,window.innerHeight);
+        p = new paddle(0);
+        p2 = new paddle(width-15);
+        b = new ball()
+}
+
+class paddle{
+        constructor(x){
+                this.x = x;
+                this.y = height/2 - 150;
+                this.a = 0;
+                this.xspeed = 0;
+                this.yspeed = 0;
+                this.up = true;
+                this.down = true;
+        }
+
+        moveup(){
+                this.x = this.x + this.xspeed * 10;
+        }
+        movedown(){
+                this.y = this.y + this.yspeed * 10;
+        }
+
+        key(xspeed,yspeed){
+                this.xspeed = xspeed;
+                this.yspeed = yspeed;
+        }
+
+
+
+        draw(){
+                rect(this.x,this.y,15,150);
+                if(this.y + 150 >= height){
+                        this.down = false;
+                        this.yspeed = 0;
+                }else if(this.y <= 0){
+                        this.up = false;
+                        this.yspeed = 0;
+                }
+
+        }
+}
+
+
+class ball{
+        constructor(){
+                this.x = width/2;
+                this.y = height/2;
+                this.r = 25;
+                this.ax = -5;
+                this.ay = floor(random(4));
+        }
+
+        draw(){
+                ellipse(this.x,this.y,this.r,this.r);
+        }
+
+        move(){
+                this.x += this.ax;
+                this.y += this.ay;
+                this.paddler();
+                this.wallr();
+        }
+
+        paddler(){
+                if((this.y >= p.y && this.y <= p.y + 150) || (this.y >= p2.y && this.y <= p2.y + 150)){
+                        if(this.x - this.r === p.x + 15){
+                                this.ax = -this.ax;
+                                this.ay = -floor(random(4));
+                        }
+
+                        if(this.x + this.r === p2.x){
+                                this.ax = -this.ax;
+                                this.ay = floor(random(4));
+                        }
+                }
+        }
+
+        wallr(){
+                if(this.y + this.r >= height){
+                        this.ay = -this.ay;
+                }else if(this.y - this.r <= 0){
+                        this.ay = -this.ay;
+                }else if(this.x - this.r <= 0){
+                        this.reset();
+                }else if(this.x + this.r >= width){
+                        this.reset();
+                }
+
+
+        }
+
+        reset(){
+                this.x = width/2;
+                this.y = height/2;
+                this.ax = -5;
+                this.ay = floor(random(4));
+        }
+}
+
+function keyPressed(){
+        if(keyCode === UP_ARROW && p.up === true){
+                p.moveup();
+                p.down = true;
+        }else if(keyCode === DOWN_ARROW && p.down === true){
+                p.movedown();
+                p.up = true;
+        }
+
+        if (keyCode === 87 && p2.up === true){
+                p2.key(0,-1);
+                p2.down = true;
+        }else if(keyCode === 83 && p2.down === true){
+                p2.key(0,1);
+                p2.up = true;
+        }
+}
+
+function draw(){
+        background(255);
+        p.draw();
+        p2.draw();
+        b.draw();
+        b.move();
+}
---
+++ empty-example/snake/snakesketch.js
@@ -0,0 +1,93 @@
+var food;
+var x;
+var y;
+function setup() {
+        x = window.innerWidth;
+        y = window.innerHeight;
+        y = y - 26
+        createCanvas(x, y);
+        frameRate(15);
+        s = new snake();
+        put();
+}
+
+function put(){
+        var r = floor(x/20);
+        //console.log(r);
+        var c = floor(y/20);
+        food = createVector((floor(random(r))*20),(floor(random(c))*20));
+}
+
+function snake(){
+        this.x = 0;
+        this.y = floor(height/2) - 20;
+        this.xspeed = 0;
+        this.yspeed = 0;
+        this.l = 0;
+        this.tail = [];
+        //tail.push([this.x,this.y]);
+        this.update = function(){
+
+                for(i = 0; i < this.tail.length-1;i++){
+                        this.tail[i] = this.tail[i+1];
+                }
+                this.tail[this.l-1] = createVector(this.x,this.y);
+
+                if(this.x + this.xspeed < width && this.x + this.xspeed >=0){
+                        this.x = this.x + this.xspeed;
+                }
+                if(this.y + this.yspeed < height && this.y + this.yspeed >=0){
+                        this.y = this.y + this.yspeed;
+                }
+
+                for(i = 0;i<this.tail.length;i++){
+                        var d = dist(this.x,this.y,this.tail[i].x,this.tail[i].y);
+                        if(d<1){
+                                this.tail = [];
+                                this.l = 0;
+                        }
+                }
+        }
+
+        this.show = function(){
+                fill(255);
+                for(i = 0;i< this.l ; i++){
+                        rect(this.tail[i].x,this.tail[i].y,20,20);
+                }
+                rect(this.x,this.y,20,20);
+        }
+
+        this.key = function(x,y){
+                this.xspeed = x*20;
+                this.yspeed = y*20;
+        }
+
+        this.eat = function(){
+                var d = dist(this.x,this.y,food.x,food.y)
+                if(d<1){
+                        this.l++;
+                        put();
+                }
+        }
+}
+
+function keyPressed(){
+        if(keyCode === UP_ARROW){
+                s.key(0,-1);
+        }else if(keyCode === LEFT_ARROW){
+                s.key(-1,0);
+        }else if(keyCode === RIGHT_ARROW){
+                s.key(1,0);
+        }else if(keyCode === DOWN_ARROW){
+                s.key(0,1);
+        }
+}
+
+function draw() {
+        background(255);
+        s.update();
+        s.show();
+        s.eat();
+        fill(134,0,0);
+        rect(food.x,food.y,20,20);
+}
